18e651a9d5019208d378ca33103b55265550f613
rider <ridebus82@nate.com>
2025-01-17 14:28:04 +0900

    250117 asdlfjasildjfgogogogogo

--- routes/zaipre.js
+++ routes/zaipre.js
@@ -21,13 +21,15 @@ zapierRouter.get('/', (req, res) => {
 
 zapierRouter.post('/', async (req, res) => {
     console.log('일단 들어 왔니?!?!?!??!');
-    
+
     let status = true;
     const body = req.body;
     console.log(body);
-    
+
     try {
-        const get_temp_phone = body['raw__phone_number'];
+
+        const dbName = body['_1']
+        const get_temp_phone = body['_2'];
         let get_phone = get_temp_phone
         try {
             get_phone = get_temp_phone.replace('+82', '').replace(/[^0-9]/g, "");
@@ -40,7 +42,7 @@ zapierRouter.post('/', async (req, res) => {
 
         const nowStr = moment().format('YYYY-MM-DD HH:mm:ss');
 
-        const get_form_name = body['form_name']
+        const get_form_name = body['_3']
         var reFormName = get_form_name.replace(/[a-zA-Z\(\)\-\s]/g, '')
 
         // 조회 먼저 해 보고 만약 기존 DB와 겹치면 패스하기
@@ -64,7 +66,7 @@ zapierRouter.post('/', async (req, res) => {
         const chkFormInSiteList = chkFormInSiteListData[0][0]
 
         console.log(chkFormInSiteList);
-        
+
 
 
         if (!chkFormInSiteList) {
@@ -77,26 +79,42 @@ zapierRouter.post('/', async (req, res) => {
         let etcValuesStr = '';
         let addEtcMessage = '';
 
-        for (const key in body) {
-            if (key.includes('raw__etc1')) {
-                etcInsertStr = etcInsertStr + `, af_mb_etc1`;
-                etcValuesStr = etcValuesStr + `, '${body[key]}'`;
-                addEtcMessage = addEtcMessage + `// 기타 정보1 : ${body[key]}`
-            } else if (key.includes('raw__etc2')) {
-                etcInsertStr = etcInsertStr + `, af_mb_etc2`;
-                etcValuesStr = etcValuesStr + `, '${body[key]}'`;
-                addEtcMessage = addEtcMessage + `// 기타 정보2 : ${body[key]}`
-            } else if (key.includes('raw__etc3')) {
-                etcInsertStr = etcInsertStr + `, af_mb_etc3`;
-                etcValuesStr = etcValuesStr + `, '${body[key]}'`;
-                addEtcMessage = addEtcMessage + `// 기타 정보3 : ${body[key]}`
-            }
+        if (body['_4']) {
+            etcInsertStr = etcInsertStr + `, af_mb_etc1`;
+            etcValuesStr = etcValuesStr + `, '${body['_4']}'`;
+            addEtcMessage = addEtcMessage + `// 기타 정보1 : ${body['_4']}`
+        }
+        if(body['_5']){
+            etcInsertStr = etcInsertStr + `, af_mb_etc2`;
+            etcValuesStr = etcValuesStr + `, '${body['_5']}'`;
+            addEtcMessage = addEtcMessage + `// 기타 정보1 : ${body['_5']}`
         }
 
-        const values = [reFormName, '분양', 'FB', body['raw__full_name'], get_phone, '', nowStr]
+        if (body['_6']) {
+            etcInsertStr = etcInsertStr + `, af_mb_etc3`;
+            etcValuesStr = etcValuesStr + `, '${body['_6']}'`;
+            addEtcMessage = addEtcMessage + `// 기타 정보1 : ${body['_6']}`
+        }
+        // for (const key in body) {
+        //     if (key.includes('raw__etc1')) {
+        //         etcInsertStr = etcInsertStr + `, af_mb_etc1`;
+        //         etcValuesStr = etcValuesStr + `, '${body[key]}'`;
+        //         addEtcMessage = addEtcMessage + `// 기타 정보1 : ${body[key]}`
+        //     } else if (key.includes('raw__etc2')) {
+        //         etcInsertStr = etcInsertStr + `, af_mb_etc2`;
+        //         etcValuesStr = etcValuesStr + `, '${body[key]}'`;
+        //         addEtcMessage = addEtcMessage + `// 기타 정보2 : ${body[key]}`
+        //     } else if (key.includes('raw__etc3')) {
+        //         etcInsertStr = etcInsertStr + `, af_mb_etc3`;
+        //         etcValuesStr = etcValuesStr + `, '${body[key]}'`;
+        //         addEtcMessage = addEtcMessage + `// 기타 정보3 : ${body[key]}`
+        //     }
+        // }
+
+        const values = [reFormName, '분양', 'FB', dbName, get_phone, '', nowStr]
 
         // 폼 insert 하기!!
-        formInertSql = `INSERT INTO application_form (af_form_name, af_form_type_in, af_form_location, af_mb_name, af_mb_phone, af_mb_status ${etcInsertStr}, af_created_at) VALUES (?,?,?,?,?,? ${etcValuesStr},?);`;
+        const formInertSql = `INSERT INTO application_form (af_form_name, af_form_type_in, af_form_location, af_mb_name, af_mb_phone, af_mb_status ${etcInsertStr}, af_created_at) VALUES (?,?,?,?,?,? ${etcValuesStr},?);`;
         await sql_con.promise().query(formInertSql, values)
 
         // 해당 폼네임에 저장된 담당자 리스트 찾기
@@ -105,100 +123,61 @@ zapierRouter.post('/', async (req, res) => {
         const findUser = findUserData[0];
 
         // 담당자들 에게 이메일 발송
-        for await (const goUser of findUser) {
-            const mailSubjectManager = `${reFormName} / ${body['raw__full_name']} 고객 DB 접수되었습니다.`;
-            const mailContentManager = `현장 : ${reFormName} / 이름 : ${body['raw__full_name']} / 전화번호 : ${get_phone} ${addEtcMessage}`;
-            mailSender.sendEmail(goUser.user_email, mailSubjectManager, mailContentManager);
-        }
+        // for await (const goUser of findUser) {
+        //     const mailSubjectManager = `${reFormName} / ${dbName} 고객 DB 접수되었습니다.`;
+        //     const mailContentManager = `현장 : ${reFormName} / 이름 : ${dbName} / 전화번호 : ${get_phone} ${addEtcMessage}`;
+        //     mailSender.sendEmail(goUser.user_email, mailSubjectManager, mailContentManager);
+        // }
 
         // 최고관리자에게 이메일 발송
-        const mailSubject = `(리치분양 접수) ${reFormName} 고객명 ${body['raw__full_name']} 접수되었습니다.`;
-        const mailContent = `현장: ${reFormName} / 이름 : ${body['raw__full_name']} / 전화번호 : ${get_phone} ${addEtcMessage}`;
+        const mailSubject = `(리치분양 접수) ${reFormName} 고객명 ${dbName} 접수되었습니다.`;
+        const mailContent = `현장: ${reFormName} / 이름 : ${dbName} / 전화번호 : ${get_phone} ${addEtcMessage}`;
         mailSender.sendEmail('adpeak@naver.com', mailSubject, mailContent);
         mailSender.sendEmail('changyong112@naver.com', mailSubject, mailContent);
 
-        // 현장명 찾기!!!
-        const getSiteInfoSql = `SELECT * FROM site_list WHERE sl_site_name = ?`
-        const getSiteInfoData = await sql_con.promise().query(getSiteInfoSql, [reFormName])
-        const getSiteInfo = getSiteInfoData[0][0];
 
 
-        if (getSiteInfo.sl_site_link) {
-            var siteList = getSiteInfo.sl_site_link
-        } else {
-            var siteList = '정보없음'
-        }
-
-        const receiverStr = `${get_phone} ${addEtcMessage}`
-
-        let dbName = body['raw__full_name']
-        const cleanText = dbName.replace(/[^\w\s.,!@#$%^&*()_\-+=\[\]{}|;:'"<>?\\/가-힣]/g, '');
-        const containsKoreanOrEnglish = /[A-Za-z\uAC00-\uD7A3]/.test(cleanText);
-
-        if (containsKoreanOrEnglish) {
-            dbName = cleanText
-        } else {
-            dbName = '성함 미입력'
-        }
-
-        var customerInfo = { ciName: dbName, ciCompany: '리치분양', ciSite: getSiteInfo.sl_site_name, ciSiteLink: siteList, ciReceiver: receiverStr }
-
-        // 매니저한테 알림톡 / 문자 발송
-        for (let oo = 0; oo < findUser.length; oo++) {
-            const managerPhone = findUser[oo].user_phone
-            if (managerPhone.includes('010')) {
-                customerInfo['ciPhone'] = managerPhone
-                // console.log(customerInfo);
-                try {
-                    aligoKakaoNotification_formanager(req, customerInfo)
-                } catch (e) {
-                    console.error(e.message);
-                }
 
 
-                // const resMessage = `리치분양 고객 인입 안내! ${getSiteInfo.sl_site_name} 현장 / ${dbName}님 접수되었습니다! 고객 번호 : ${receiverStr}`
-
-                // req.body = {
-                //     sender: '010-3124-1105',
-                //     receiver: managerPhone,
-                //     msg: resMessage,
-                //     msg_type: 'LMS'
-                // }
-
-                // try {
-                //     const aligo_res = await aligoapi.send(req, AuthData)
-                //     console.log(aligo_res);
-
-                // } catch (err) {
-                //     console.error(err.message);
-                // }
-
-            }
-        }
+        // // 현장명 찾기!!!
+        // const getSiteInfoSql = `SELECT * FROM site_list WHERE sl_site_name = ?`
+        // const getSiteInfoData = await sql_con.promise().query(getSiteInfoSql, [reFormName])
+        // const getSiteInfo = getSiteInfoData[0][0];
 
 
-        // 여기서 고객에게 보낼 알림톡 발송!!!
-
-        // try {
-        //     const getSendContentQuery = "SELECT * FROM site_list WHERE sl_site_name = ?"
-        //     const getSendContent = await sql_con.promise().query(getSendContentQuery, [getSiteInfo.sl_site_name])
-        //     const sendRealSiteName = getSendContent[0][0]['sl_site_realname']
-        //     const sendContent = getSendContent[0][0]['sl_sms_content']
-        //     if (sendRealSiteName && sendContent) {
-        //         const detailSendContent = {
-        //             receiver: get_phone,
-        //             ciName: body['raw__full_name'],
-        //             ciCompany: "리치분양",
-        //             ciSite: sendRealSiteName,
-        //             smsContent: sendContent
-        //         }
-        //         // aligoKakaoNotification_detail(req, detailSendContent)
-        //     }
-
-        // } catch (error) {
-        //     console.error(error.message);
+        // if (getSiteInfo.sl_site_link) {
+        //     var siteList = getSiteInfo.sl_site_link
+        // } else {
+        //     var siteList = '정보없음'
         // }
 
+        // const receiverStr = `${get_phone} ${addEtcMessage}`
+
+        
+        // const cleanText = dbName.replace(/[^\w\s.,!@#$%^&*()_\-+=\[\]{}|;:'"<>?\\/가-힣]/g, '');
+        // const containsKoreanOrEnglish = /[A-Za-z\uAC00-\uD7A3]/.test(cleanText);
+
+        // if (containsKoreanOrEnglish) {
+        //     dbName = cleanText
+        // } else {
+        //     dbName = '성함 미입력'
+        // }
+
+        // var customerInfo = { ciName: dbName, ciCompany: '리치분양', ciSite: getSiteInfo.sl_site_name, ciSiteLink: siteList, ciReceiver: receiverStr }
+
+        // // 매니저한테 알림톡 / 문자 발송
+        // for (let oo = 0; oo < findUser.length; oo++) {
+        //     const managerPhone = findUser[oo].user_phone
+        //     if (managerPhone.includes('010')) {
+        //         customerInfo['ciPhone'] = managerPhone
+        //         // console.log(customerInfo);
+        //         try {
+        //             aligoKakaoNotification_formanager(req, customerInfo)
+        //         } catch (e) {
+        //             console.error(e.message);
+        //         }
+        //     }
+        // }
         // 알림톡 발송 끝~~~~
         return res.sendStatus(200);
 
