3223687d56eac3742bc06b06ce87cd643894aec6
rider <ridebus82@nate.com>
2025-06-30 13:27:43 +0900

    250630 asfdljogogogogo

--- routes/zapier.js
+++ routes/zapier.js
@@ -209,15 +209,15 @@ zapierRouter.post('/', async (req, res) => {
                         token: result.token,
                         tpl_code: 'UA_7459',
                         sender: '010-6628-6651',
-                        receiver_1 : managerPhone,
+                        receiver_1: managerPhone,
                         subject_1: '분양정보 신청고객 알림톡',
                         message_1: `${customerInfo.ciSite}고객 유입 알림!\n\n고객명:${customerInfo.ciName}\n연락처:${customerInfo.ciReceiver}\n\n※ 상담 대기 상태입니다.\n빠르게 컨택 진행 부탁 드립니다.`,
-                        // button_1: {
-                        //     "button": [{
-                        //         "name": "채널 추가",
-                        //         "linkType": "AC"
-                        //     }]
-                        // }
+                        button_1: {
+                            "button": [{
+                                "name": "채널 추가",
+                                "linkType": "AC"
+                            }]
+                        }
                     }
 
                     const aligo_res = await aligoapi.alimtalkSend(req, AuthData)
