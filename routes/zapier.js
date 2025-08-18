3733286055e0c56fee8e966c33dbe661e34d28bd
rider <ridebus82@nate.com>
2025-08-18 18:04:36 +0900

    250818 asdfljogogogogo';

--- routes/zapier.js
+++ routes/zapier.js
@@ -164,7 +164,7 @@ zapierRouter.post('/', async (req, res) => {
         
 
         // 폼 insert 하기!!
-        const formInertSql = `INSERT INTO application_form (af_form_name, af_form_type_in, af_form_location, af_mb_name, af_mb_phone ${etcInsertStr}, af_created_at) VALUES (${reFormName},'분양','FB',${dbName},${get_phone} ${etcValuesStr},${nowStr});`;
+        const formInertSql = `INSERT INTO application_form (af_form_name, af_form_type_in, af_form_location, af_mb_name, af_mb_phone ${etcInsertStr}, af_created_at) VALUES ('${reFormName}','분양','FB','${dbName}','${get_phone}' ${etcValuesStr},${nowStr});`;
 
         console.log(formInertSql);
 
